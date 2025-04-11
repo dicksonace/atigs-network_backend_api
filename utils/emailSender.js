a046f4228527af9b1fe961f04128fe947c189e13
Dickson Awudu <dickson.awudu@shrinqghana.com>
2025-04-11 07:08:32 +0000

    img c

--- utils/emailSender.js
+++ utils/emailSender.js
@@ -23,7 +23,7 @@ const sendEmail = async (options) => {
       html: options.html,
       attachments: [{
         filename: 'logo.png',
-        path: path.join(__dirname, '../public/images/logo.jpg'),
+        path: path.join(__dirname, '../public/images/logo1.jpg'),
         cid: 'companylogo'
       }]
     };
